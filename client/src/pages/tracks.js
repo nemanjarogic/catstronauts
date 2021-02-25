b6b0381d3a99d9fbc84cccd2235dfa4ba3b2105f
Nemanja <nemanjarogic93@gmail.com>
2021-02-25 12:44:21 +0100

    Configure apollo client and execute GraphQL query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Layout } from "../components";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
- * Tacks Page is Catstronauts home page.
+ * Tracks Page is Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
